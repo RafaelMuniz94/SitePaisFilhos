06371d55dbc1d1056fc7369d96f7fea1a0c32563
Rafael Muniz <raruznh@gmail.com>
2020-01-12 18:12:42 -0300

    Adicao do .gitignore

---
+++ js/paginaPrincipal.js
@@ -0,0 +1,9 @@
+$('html').bind('mousewheel DOMMouseScroll', function (e) {
+    console.log('bingo');
+});
+// pagina = document.getElementById('pagina');
+// pagina.onscroll = new function rolarPagina(){
+    
+//     console.log("xakcmscka")
+
+// }
